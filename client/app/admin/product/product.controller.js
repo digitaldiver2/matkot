7e55db90d96afc5f87acb530e2ce48a0f1a81b95
Stijn Haers <stijn.haers@gmail.com>
2016-05-12 21:43:54 +0200

    intermediate commit

--- client/app/admin/product/product.controller.js
+++ client/app/admin/product/product.controller.js
@@ -5,31 +5,102 @@ class ProductComponent {
   constructor($scope, $http, $stateParams, $location) {
     this.$scope = $scope;
     this.$http = $http;
+    this.$location = $location;
     this.id = $stateParams.id;
     this.$scope.product = {};
-    this.$scope.groups = [];
-    this.$scope.productcategories = [];
+    this.$scope.groups = undefined;
+    this.$scope.productcategories = undefined;
+    this.loaded = {};
   }
 
   $onInit() {
+    if (this.id) {
+      this.$http.get('/api/products/' + this.id).then(response => {
+        this.$scope.product = response.data;
+        this.loaded['product'] = true;
+      });
+    }
+
   	this.$http.get('/api/productfamilies').then(response => {
   		this.$scope.productcategories = response.data;
+      this.loaded['productfamilies'] = true;
+      this.helpsync();
   	});
 
   	this.$http.get('/api/usergroups').then(response => {
   		this.$scope.groups = response.data;
+      this.loaded['usergroups'] = true;
+      this.helpsync();
   	});
   }
 
+  helpsync () {
+    if (this.loaded['product']) {
+      if (this.loaded['productfamilies']) {
+        this.setproductfamilies();
+      }
+
+      if (this.loaded['usergroups']) {
+        this.setgroups();
+      }
+    }
+  }
+
   submit() {
+    this.updategroups();
+    this.updateproductfamilies();
+
+    console.dir(this.$scope.product);
+
   	if (this.id) {
-		//update
-		this.$scope.put('/api/product/' + this.id, this.$scope.product);
+  		//update
+  		this.$http.put('/api/products/' + this.id, this.$scope.product);
   	} else {
   		//new product
-		this.$scope.post('/api/product/', this.$scope.product);
+		  this.$http.post('/api/products/', this.$scope.product);
   	}
   	this.$location.path ('/admin/products');
+  }
+
+  setgroups() {
+    for (var i=0; i<this.$scope.product.visiblegroups.length; i++) {
+      for (var j=0; j<this.$scope.groups.length; j++) {
+        if (this.$scope.product.visiblegroups[i]._id == this.$scope.groups[j]._id) {
+          this.$scope.groups[j].checked = true;
+          break;
+        }
+      }
+    }
+  }
+
+  setproductfamilies() {
+    for (var i=0; i<this.$scope.product.productfamily.length; i++) {
+      for (var j=0; j<this.$scope.productcategories.length; j++) {
+        if (this.$scope.product.productfamily[i]._id == this.$scope.productcategories[j]._id) {
+          this.$scope.productcategories[j].checked = true;
+          break;
+        }
+      }
+    }
+  }
+
+  updategroups() {
+    this.$scope.product.visiblegroups = [];
+    for (var j=0; j<this.$scope.groups.length; j++) {
+      console.dir(this.$scope.groups[j]);
+      if (this.$scope.groups[j].checked) {
+        this.$scope.product.visiblegroups.push(this.$scope.groups[j]._id);
+      }
+    }
+  }
+
+  updateproductfamilies() {
+    this.$scope.product.productfamily = [];
+    for (var j=0; j<this.$scope.productcategories.length; j++) {
+      if (this.$scope.productcategories[j].checked) {
+        this.$scope.product.productfamily.push(this.$scope.productcategories[j]._id);
+      }
+    }
   }
 }
 
